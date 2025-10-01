bde80f0b797a77576ba31b68b4122a2e97cb9e05
Mozzie-org <alirzamhp79@gmail.com>
2025-10-01 20:14:53 +0330

    JavaScript file has been updated

--- JS/Elements.js
+++ JS/Elements.js
@@ -6,6 +6,14 @@ export let spinnerSearchEl = document.querySelector('.spinner--search');
 export let resultNumberEl = document.querySelector('.number_of_results');
 export let jobItemsContainer = document.querySelector('.job_result_main');
 
+// Related to the Job Details
+export let spinnerJobDetailsEl = document.querySelector('.spinner--job-details');
+export let jobItemEl = document.querySelector('.job_result_main');
+export let beforeUploadingJobDetailsEl = document.querySelector('.before_uploading');
+export let afterUploadingJobDetailsEl = document.querySelector('.after_uploading');
+export let totalAvailableJobEl = document.querySelector('.total_job_avialable_footer');
+
+
 
 // The base API url
 export const Base_URL = 'https://bytegrad.com/course-assets/js/2/api/jobs';
--- script.js
+++ script.js
@@ -1,87 +1,17 @@
 import './JS/Components/searchComponent.js';
+import './JS/Components/JobDetailsComponent.js';
+import {totalAvailableJobEl, Base_URL} from './JS/Elements.js';
 
-/*
-<div class="after_uploading_header">
-                    <button class="apply_btn">APPLY 
-                        <span>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
-                            </svg>
-                        </span>
-                    </button>
-                    <div class="after_uploading_header_job">
-                        <h4 class="after_uploading_header_job_title">Frontend Developer - React</h4>
-                        <p class="after_uploading_header_company">AT Security</p>
-                    </div>
-                    <div class="company_badge_container">
-                        <p class="company_badge">AT</p>
-                    </div>
-                </div>
-                <div class="after_uploading_main">
-                    <div class="after_uploading_main_job_description">
-                        <div class="after_uploading_main_job_description_day_bookmark">
-                            <span class="day_ago">#4d</span>
-                            <a href="#" class="bookmark">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                                    <path fill-rule="evenodd" d="M6.32 2.577a49.255 49.255 0 0 1 11.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 0 1-1.085.67L12 18.089l-7.165 3.583A.75.75 0 0 1 3.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93Z" clip-rule="evenodd" />
-                                </svg>
-                            </a>
-                        </div>
-                        <div class="after_uploading_main_job_description_other">
-                            <p class="description">
-                                Are you passionate about building software that makes a difference? Do you enjoy collaboration with a team of fun and smart creative people?
-                            </p>
-                            <div class="job_item_job_summarized_detail_line_3">
-                                <div class="time">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                                        <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z" clip-rule="evenodd" />
-                                    </svg>
-                                    <span>Full Time</span>
-                                </div>
-                                <div class="salary">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                                        <path d="M4.5 3.75a3 3 0 0 0-3 3v.75h21v-.75a3 3 0 0 0-3-3h-15Z" />
-                                        <path fill-rule="evenodd" d="M22.5 9.75h-21v7.5a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3v-7.5Zm-18 3.75a.75.75 0 0 1 .75-.75h6a.75.75 0 0 1 0 1.5h-6a.75.75 0 0 1-.75-.75Zm.75 2.25a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z" clip-rule="evenodd" />
-                                    </svg>
-                                    <span>$90,000+</span>
-                                </div>
-                                <div class="location">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                                        <path fill-rule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clip-rule="evenodd" />
-                                    </svg>
-                                    <span>Global</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="skills_container">
-                            <div class="row1">
-                                <div class="qualifications">
-                                    <p class="qualifications_header">Qualifications</p>
-                                    <p class="qualifications_subheader">Other qualifications may apply</p>
-                                </div>
-                                <div class="qualifications_items">
-                                    <span>JavaScript</span>
-                                    <span>React</span>
-                                </div> 
-                            </div>
-                            <div class="row2">
-                                <div class="company_reviews">
-                                    <p class="company_reviews_header">Company reviews</p>
-                                    <p class="company_reviews_subheader">Recent things people are saying</p>
-                                </div>
-                                <div class="company_reviews_items">
-                                    <span></span>
-                                    <p class="review_item">I had a great internship.</p>
-                                    <p class="review_item">Good stepping stone.</p>
-                                    <p class="review_item">Well-run and organized company.</p>
-                                    <p class="review_item">Great coworkers & opportunities.</p>
-                                </div>
-                            </div>
-                    </div>
-                    <span class="main_footer_separator"></span>
-                    <div class="after_uploading_footer">
-                        <p>If possible, please reference that you found the job on rmtDev, we would really appreciate it!</p>
-                    </div>
-                </div>
-*/
+fetch(Base_URL)
+.then(res => {
+    if(!res.ok){
+        console.log("Server Doesn't Responded");
+        return;
+    }
+    return res.json();
+})
+.then(data => {
+    const totalJobs = data.jobItems.length;
+    totalAvailableJobEl.textContent = totalJobs;
+})
+
